3e62bd17022337464085175fdbb0b9e1c5d3c99c
Vivek Patel <vivek.patel@procore.com>
2016-07-12 14:04:32 -0700

    Made some changes to make the tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   get  '/help', to: 'static_pages#help'
 
   get  '/about', to: 'static_pages#about'
